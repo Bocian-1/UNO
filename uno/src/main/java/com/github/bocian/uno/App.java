56c511b16e67cd626304027913a819680f801e40
Bocian-1 <mikolajhendrys@gmail.com>
2025-04-09 19:01:42 +0200

    Initial Commit

---
+++ uno/src/main/java/com/github/bocian/uno/App.java
@@ -0,0 +1,21 @@
+package com.github.bocian.uno;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX with Maven!");
+        Scene scene = new Scene(label, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ uno/src/main/java/com/github/bocian/uno/Main.java
@@ -0,0 +1,5 @@
+package com.github.bocian.uno;
+
+public class Main {
+
+}
